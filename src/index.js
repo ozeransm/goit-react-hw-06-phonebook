3f67f5cac94ea3c38f58794b24156980cb7a793a
Serhii Ozeran <ozeransm@gmail.com>
2023-08-05 08:02:51 +0300

    update import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'components/redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
+// import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
